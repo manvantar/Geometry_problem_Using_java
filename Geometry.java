0f3785a4378a5286b3a78ee8e7bd37a391d001f8
manvantar <manukvshetty@gmail.com>
2021-03-25 17:52:43 +0530

    calculating line eqution

---
+++ Geometry.java
@@ -0,0 +1,20 @@
+package Day5;
+
+import java.util.*;
+public class Geometry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float x1=1;
+		float x2=2;
+		float y1=3;
+		float y2=4;
+		
+		float length=(float) Math.sqrt(Math.pow((x1-x2),2)+ Math.pow((y2-y1),2)); 
+		System.out.println(length);
+		
+		
+	}
+
+}
