a8dde6a31f36db9d1fe3b956a6a628ce01d7ecad
manvantar <manukvshetty@gmail.com>
2021-03-25 22:49:02 +0530

    check two lengths of lines USING COMPARETO METHOD

--- Geometry.java
+++ Geometry.java
@@ -7,13 +7,15 @@ public class Geometry {
 		// TODO Auto-generated method stub
 		
 		float x1=1,y1=3,x2=2,y2=4;
-		float p1=4,q1=5,p2=7,q2=-8;
+		float p1=1,q1=3,p2=2,q2=4;
 		
 		float length1=(float) Math.sqrt(Math.pow((x1-x2),2)+ Math.pow((y2-y1),2)); 
 		
 		float length2=(float) Math.sqrt(Math.pow((p1-p2),2)+ Math.pow((q2-q1),2)); 
 		
+		String l1=""+length1;
 		System.out.println(length1 +" "+length2 );
+		String l2=""+length2;
 		
 		if(length1==length2) {
 		System.out.println("Both are equal");				
@@ -23,6 +25,8 @@ public class Geometry {
 			System.out.println("Both are not equal");	
 			
 		}
+		System.out.println(l1.compareTo(l2));
+		
 		
 		
 	}
